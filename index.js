a29fb94b59cd19bbedcce22d4359d53035580c31
Michael Frank <mfrankwork@gmail.com>
2020-03-17 15:45:43 -0400

    updates: Switch to async handler

--- index.js
+++ index.js
@@ -2,24 +2,25 @@
 
 const insult = require('shakespeare-insult');
 
-exports.handler = function(event, context, callback) {
-  let body = JSON.parse(event.body);
+exports.handler = async event => {
+  let response;
+
+  const body = JSON.parse(event.body);
   const companyId = body.creator.company.id;
 
   if (companyId == process.env.companyId) {
     const insult = insult.random();
     response = {
       statusCode: 200,
-      body: insult
+      body: JSON.stringify(insult)
     };
     console.log('sending this insult', insult);
-    callback(null, response);
   } else {
     response = {
       statusCode: 403,
       body: 'Access Denied'
     };
     console.log('access denied');
-    callback(null, response);
   }
+  return response;
 };
